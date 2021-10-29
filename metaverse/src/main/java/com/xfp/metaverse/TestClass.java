da25e3c2057d301fc6673d9e57c25b993346d4b8
SirongWu <52275936+SirongWu@users.noreply.github.com>
2021-10-30 00:02:29 +0800

    init submit

---
+++ metaverse/src/main/java/com/xfp/metaverse/TestClass.java
@@ -0,0 +1,13 @@
+package com.xfp.metaverse;
+
+import javax.annotation.PostConstruct;
+
+public class TestClass {
+
+    @PostConstruct
+    void init(){
+        System.out.println("HelloWorld");
+    }
+
+
+}
---
+++ metaverse/src/main/java/com/xfp/metaverse/XfpMetaverseApplication.java
@@ -0,0 +1,13 @@
+package com.xfp.metaverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XfpMetaverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XfpMetaverseApplication.class, args);
+	}
+
+}
